baabcf5011ca0bf2108a41f16a681277c5fc3eb8
Singh <437038@cognizant.com>
2019-06-13 02:39:56 +0530

    working till id sent to java script

--- public/js/script.js
+++ public/js/script.js
@@ -16,8 +16,7 @@ function addFavourite() {
 
 function requestListener() {
 	var data = JSON.parse(this.responseText);
-	//test1();
-
+	
 	console.log(data[0].id);
 }
 
@@ -25,74 +24,21 @@ function requestError(error) {
 	console.log('We have an issue', error);
 }
 
-function test1(){
-	var elm = document.getElementById('moviesCarousel_inner');
-	//var newElement = document.createElement('div');
-	elm.innerHTML = createmyElements();
-	//elm.appendChild(newElement);
-	console.log( createmyElements());
-}
+$(document).ready(function(){  
+	var m = ["images/don.jpg","images/lagaan.jpg"];
+  for(var i=0 ; i< m.length ; i++) {
+    $('<div id ="MS_'+i+'" class="card item" ><img src="'+m[i]+'"alt="don"><div class="card-body"><a href="#" class="btn btn-primary" style="width:100%;" onclick="test('+i+')">Add to favorite</a></div><div class="carousel-caption"></div></div>').appendTo('.carousel-inner');
+    $('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
 
-function createmyElements(){
-   return [
-           '<div id="moviesCarousel" class="carousel slide" style="width:50%;" data-ride="carousel">',
-						'<ol class="carousel-indicators">',
-							'<li data-target="#moviesCarousel" data-slide-to="0" class="active"></li>',
-							'<li data-target="#moviesCarousel" data-slide-to="1"></li>',
-							'<li data-target="#moviesCarousel" data-slide-to="2"></li>',
-						'</ol>',
-						'<div class="carousel-inner">',
-							'<div class="card item active">',
-								'<img src="images/don.jpg" alt="don" style="width:100%">',
-								'<div class="card-body">',
-									'<a href="#" class="btn btn-primary" onclick="getMovies()">Add to favorite</a>',
-								'</div>',
-							'</div>',
-							'<div class="item">',
-								'<img src="images/lagaan.jpg" alt="lagaan" style="width:100%;">',
-								'<div class="card-body">',
-									'<a href="#" class="btn btn-primary">Add to favorite</a>',
-								'</div>',
-							'</div>',
-							'<div class="item">',
-								'<img src="images/rawDeal.jpg" alt="rawDeal" style="width:100%;">',
-								'<div class="card-body">',
-									'<a href="#" class="btn btn-primary">Add to favorite</a>',
-								'</div>',
-							'</div>',
-						'</div>',
-						'<a class="left carousel-control" href="#myCarousel" data-slide="prev">',
-							'<span class="glyphicon glyphicon-chevron-left"></span>',
-							'<span class="sr-only">Previous</span>',
-						'</a>',
-						'<a class="right carousel-control" href="#myCarousel" data-slide="next">',
-							'<span class="glyphicon glyphicon-chevron-right"></span>',
-							'<span class="sr-only">Next</span>',
-						'</a>',
-				'</div>'
-          ].join('\n');
+  }
+  $('.item').first().addClass('active');
+  $('.carousel-indicators > li').first().addClass('active');
+  $('#carousel-example-generic').carousel();
+});
+
+function test(hello){
+	console.log(hello);
 }	
-
-function addDivElements(){
-	var innerHtml="<div class=\"card item\"><img src=\"images/don.jpg\" alt=\"don\" style=\"width:100%;\">"
-	+"<div class=\"card-body\">"
-	+"<a href=\"#\" class=\"btn btn-primary\">Add to favorite</a>"
-	+"</div>"
-	+"</div>";
-	return innerHtml;
-}
-
-function addDivElementsActive(){
-	var innerHtml="<div class=\"card item active\"><img src=\"images/don.jpg\" alt=\"don\" style=\"width:100%;\">"
-	+"<div class=\"card-body\">"
-	+"<a href=\"#\" class=\"btn btn-primary\">Add to favorite</a>"
-	+"</div>"
-	+"</div>";
-	return innerHtml;
-}
-
-
-	
 
 
 
